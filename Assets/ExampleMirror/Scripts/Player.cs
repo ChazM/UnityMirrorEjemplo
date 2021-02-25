65940f104e2137220f510c719dcf0f58dbd5f5d9
Realform Studio <sl4sh8@gmail.com>
2021-02-24 21:35:18 -0600

    Hello world Multiplayer with UnityMirror

---
+++ Assets/ExampleMirror/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class Player : NetworkBehaviour
+{
+    [SerializeField]
+        Vector3 PlayerPos;
+   
+ void handleMovements(){
+     if(isLocalPlayer){
+         float Move_H = Input.GetAxis("Horizontal");
+         float Move_V = Input.GetAxis("Vertical");
+
+         Vector3 Movement =  new Vector3 (Move_H, 0, Move_V);         
+         transform.position = transform.position + Movement;
+         PlayerPos = transform.position;
+     }
+ }
+
+ void Update (){
+
+     handleMovements();
+ }
+
+}
